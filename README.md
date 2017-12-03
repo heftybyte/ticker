eeb8dea337680e1d268168c8972577f5ff76243e
Esco Obong <esco@hbyte.com>
2017-12-03 03:57:46 -0500

    store ticker prices in influx

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ticker
+# Ticker
+
 Historical &amp; Real-time Token Price Data
+
+RFC: https://docs.google.com/document/d/1pf6wQ2GMgntiYjSX-jK0R3xBpK-h1Xvo8-Fr3L2eUO4/edit#
---
+++ index.js
@@ -0,0 +1,39 @@
+import {setInterval} from 'timers';
+import {getPrices} from './lib/prices';
+import db from './lib/db';
+
+const pollDuration = process.env.POLL_DURATION || 30000;
+
+const storeCurrentPrices = async () => {
+	const prices = await getPrices();
+	// console.log('got prices', prices);
+
+	const points = prices.map(price=>({
+		measurement: 'ticker_prices',
+		tags: {
+			fsym: price.fsym,
+			tsym: price.tsym
+		},
+		fields: {
+			price: price.price,
+			volume_24_hr: price.volume_24_hr,
+			change_pct_24_hr: price.change_pct_24_hr,
+			market_cap: price.market_cap
+		},
+		timestamp: price.timestamp
+	}));
+
+	console.log(`saving ${points.length} price points`);
+
+	let err;
+	await db.writePoints(points).catch(e=>err=e);
+
+	if (err) {
+		console.error('an error occured', err);
+	} else {
+		console.log(`saved ${points.length} price points`);
+	}
+}
+
+storeCurrentPrices();
+setInterval(storeCurrentPrices, pollDuration);
