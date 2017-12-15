1ae49789f6fd21660f67a2b06f40a498821a9d39
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2017-12-15 04:28:54 +0100

    updated deploy

--- deploy/ecosystem.config.js
+++ deploy/ecosystem.config.js
@@ -20,6 +20,7 @@ module.exports = {
       user : 'ubuntu',
       host : '52.89.161.39',
       ref  : 'origin/master',
+      "key"  : "/Users/Samparsky/Sites/hbyte/server/parity-server.pem", // path to the private key to authenticate
       repo : 'git@github.com:heftybyte/ticker.git',
       "ssh_options": ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
       path : '/home/ubuntu/ticker/',
