09e2df9c08818c64eb9872447f765d865ebe405e
Omidiora Samuel <Samparsky@MACBOOKs-MacBook-Pro.local>
2017-12-15 15:45:40 +0100

    updated deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,13 @@
 echo "---- Running npm install ----"
 npm install
-echo " --- remove current build ---"
-rm -R dist/
 echo "--- Running npm bild ----"
 npm run build
-echo "---- cd dist ----"
-cd dist/
 echo " --- copy env --"
 cp /home/ubuntu/env/.env /home/ubuntu/ticker/current/dist/
+echo " ---  swap directory --- "
+mv current/ old_dist/ && mv dist/ current/
+echo "---- cd current ----"
+cd current/
 echo "--- pm2 restart -----"
 pm2 startOrRestart deploy/ecosystem.config.js -i 4 --update-env
 
