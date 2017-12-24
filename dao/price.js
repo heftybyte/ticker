13b69eb97a78070ca78ccde4fc85bb438a4a56c1
Esco Obong <esco@hbyte.com>
2017-12-24 13:52:31 -0500

    add recent method for price data <= 1 week

--- dao/price.js
+++ dao/price.js
@@ -6,6 +6,23 @@ const formatChart = (price={}) =>({
 	y: Number(price.close)
 })
 
+const periodInterval = {
+	'1d': '5m',
+	'1w': '10m',
+	'1m': '1d',
+	'3m': '1d',
+	'1y': '1d',
+	'all': '1d'
+}
+const periodMap = {
+	'1d': '1d',
+	'1w': '1w',
+	'1m': '31d',
+	'3m': '93d',
+	'1y': '366d',
+	'all': '5000d'
+}
+
 export const now = async (fsym, tsym) => {
 	const query = `
 		select * from ticker_prices
@@ -13,6 +30,7 @@ export const now = async (fsym, tsym) => {
 		order by time desc
 		limit 1
 	`;
+	console.log('now', query)
 	let err;
 	const rows = await influx.query(query, { precision: Precision.Milliseconds }).catch(e=>err=e);
 	if (err) {
@@ -34,7 +52,7 @@ export const nowMulti = async (fsyms, tsyms) => {
 		order by time desc
 		limit 1
 	`;
-	console.log(query)
+	console.log('nowMulti', query)
 	let err;
 	const rows = await influx.query(query, { precision: Precision.Milliseconds }).catch(e=>err=e);
 	if (err) {
@@ -50,24 +68,64 @@ export const nowMulti = async (fsyms, tsyms) => {
 	return map;
 }
 
-const periodInterval = {
-	'1d': '5m',
-	'1w': '10m',
-	'1m': '1d',
-	'3m': '1d',
-	'1y': '1d',
-	'all': '1d'
-}
-const periodMap = {
-	'1d': '1d',
-	'1w': '1w',
-	'1m': '31d',
-	'3m': '93d',
-	'1y': '366d',
-	'all': '5000d'
+const recent = async (fsym, tsym, period='1d') => {
+	period = periodMap[period]
+	const interval = interval || periodInterval[period] || '1d'
+	const query = `
+		select LAST(volume_24_hr) as volume, LAST(change_pct_24_hr) as change, LAST(market_cap) as market_cap, MAX(price) as high, MIN(price) as low, FIRST(price) as open, LAST(price) as close from ticker_prices
+		where fsym = ${escape.stringLit(fsym)}
+		and tsym = ${escape.stringLit(tsym)}
+		and time > now() - ${period}
+		group by time(${interval})
+		order by time desc
+	`;
+	console.log('recent', query)
+	let err;
+	const rows = await influx.query(query, { precision: Precision.Milliseconds }).catch(e=>err=e);
+	if (err) {
+		throw err;
+	}
+	return {
+		[fsym]: {
+			[tsym]: rows
+		}
+	};
 }
 
-export const hist = async ({fsym, tsym, period, interval, start=0, end=0, format='raw'}) => {
+const recentMulti = async (fsyms, tsyms, period='1d') => {
+	period = periodMap[period]
+	const interval = interval || periodInterval[period] || '1d'
+	const query = `
+		select LAST(volume_24_hr) as volume, LAST(change_pct_24_hr) as change, LAST(market_cap) as market_cap, MAX(price) as high, MIN(price) as low, FIRST(price) as open, LAST(price) as close from ticker_prices
+		where fsym =~ /^${fsyms.join('$|^')}$/
+		and tsym =~ /^${tsyms.join('$|^')}$/
+		and time > now() - ${period}
+		group by fsym, tsym, time(${interval})
+		order by time desc
+	`;
+	console.log('recentMulti', query)
+	let err;
+	const rows = await influx.query(query, { precision: Precision.Milliseconds }).catch(e=>err=e);
+	if (err) {
+		throw err;
+	}
+	const map = {};
+	fsyms.forEach((fsym)=>{
+		map[fsym] = {}
+		tsyms.forEach((tsym)=>{
+			map[fsym][tsym] = []
+		})
+	})
+	rows.forEach((row)=>{
+		map[row.fsym][row.tsym].push(row)
+	});
+	return map;
+}
+
+export const hist = async ({fsym, tsym, period='1d', interval, start=0, end=0, format='raw'}) => {
+	if (period === '1d' || period === '1w') {
+		return await recent(fsym, tsym, period)
+	}
 	start = new Date(Number(start));
 	end = Number(end) ? new Date(Number(end)) : new Date();
 	interval = interval || periodInterval[period] || '1d'
@@ -109,7 +167,10 @@ export const hist = async ({fsym, tsym, period, interval, start=0, end=0, format
 	return map;
 }
 
-export const histMulti = async ({fsyms, tsyms, period, interval, start=0, end=0, format='raw'}) => {
+export const histMulti = async ({fsyms, tsyms, period='1d', interval, start=0, end=0, format='raw'}) => {
+	if (period === '1d' || period === '1w') {
+		return await recentMulti(fsyms, tsyms, period)
+	}
 	start = new Date(Number(start));
 	end = Number(end) ? new Date(Number(end)) : new Date();
 	interval = interval || periodInterval[period] || '1d'
@@ -131,7 +192,7 @@ export const histMulti = async ({fsyms, tsyms, period, interval, start=0, end=0,
 		group by fsym, tsym, time(${interval})
 		order by time asc
 	`;
-	console.log(query)
+	console.log('histMulti', query)
 	let err;
 	const rows = await influx.query(query, { precision: Precision.Milliseconds }).catch(e=>err=e);
 	if (err) {
